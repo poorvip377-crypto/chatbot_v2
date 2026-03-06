331cdcc4070f4b104d9f846c0d2578ad83ad66ea
imrageshgr <rageshgr7010@gmail.com>
2026-03-06 13:50:30 +0530

    update

---
+++ ai_teacher/chatbot/views.py
@@ -0,0 +1,28 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from django.shortcuts import render
+
+load_dotenv()
+
+api_key = os.getenv("GOOGLE_API_KEY")
+
+client = genai.Client(api_key=api_key)
+
+
+def home(request):
+
+    answer = ""
+
+    if request.method == "POST":
+        question = request.POST.get("question")
+
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=question
+        )
+
+        answer = response.text
+
+    return render(request, "chatbot/index.html", {"answer": answer})
+print(api_key)
